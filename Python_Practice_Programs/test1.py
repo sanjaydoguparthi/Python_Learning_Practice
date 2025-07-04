5f813c72430b34536682c5031ac39f1f717eceba
sanjaydoguparthi <sanjaydoguparthi2001@gmail.com>
2025-07-04 14:06:44 +0530

    working on dictionaries and lists

--- Python_Practice_Programs/test1.py
+++ Python_Practice_Programs/test1.py
@@ -1,16 +1,45 @@
-def add(num1,num2):  ##  add number of arguments
-    result = num1+num2
-    print('result :', result)
-add(55,20)
-add(60,40)
-add(25,25)
+lst = ["sanjay","kalyan","lalith kumar","vinay kumar","rahul"]# create a list
+print(type(lst)) # type of list
+print(lst)
+lst.append('mahesh') #one more string attache in the list
+print(lst)
+lst.extend(['ashok']) # extenstion in the list
+print(lst)
 
-def sayhello(name= 'user'):# function name as a argument
-    print('hello',name)
-    print('good morning')
+print(" \n positive indexing of the list ")
+print("index[0]:",lst[0]) # positive indexing of the list 
+print("index[1]:",lst[1])
+print("index[2]:",lst[2])
+print("index[3]:",lst[3])
+print("index[4]:",lst[4])
+print("index[5]:",lst[5])
+print("index[6]:",lst[6])
 
-user_name = ('avinash')
-sayhello(user_name)
-sayhello('sanjay')
-sayhello('kalyan')
-sayhello()
+print(" \n negetive indexing of list")
+print("index[0]:",lst[0]) # positive indexing of the list 
+print("index[-1]:",lst[-1])
+print("index[-2]:",lst[-2])
+print("index[-3]:",lst[-3])
+print("index[-4]:",lst[-4])
+print("index[-5]:",lst[-5])
+print("index[-6]:",lst[-6])
+
+print(" \n positive slicing of the list ")
+
+print(lst[::2])
+print(lst[::5])
+print(lst[2::3])
+print(lst[::3])
+print(lst[2::4])
+
+print(" \n negetive slicing of the list ")
+
+print(lst[::-2])
+print(lst[::-5])
+print(lst[-2::-3])
+print(lst[::-3])
+print(lst[-2::4])
+
+
+
+
