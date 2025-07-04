93391e0bcc561575b461ba61006c3af252bcbf32
sanjaydoguparthi <sanjaydoguparthi2001@gmail.com>
2025-07-04 18:11:33 +0530

    working on sets

---
+++ sets.py
@@ -0,0 +1,9 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+... set = {1,3,4,5,6,8,6,7,8,3,2} # create set with having duplicate values
+... print(var_set)
+... print(type(var_set))
+SyntaxError: multiple statements found while compiling a single statement
+>>> set = {1,3,4,5,6,8,6,7,8,3,2} # create set with having duplicate values
+... print(var_set)
